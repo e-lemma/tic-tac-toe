938a8706301372dfb0477550abf40be9b1f02fe5
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-03-20 19:20:43 +0000

    Add working console version

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const gameBoard = (function () {
-  let board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ];
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  (function createBoard() {
+    for (let i = 0; i < rows; i++) {
+      board[i] = [];
+      for (let j = 0; j < columns; j++) {
+        board[i].push(square());
+      }
+    }
+  })();
 
   const coordMap = {
     a1: [2, 0],
@@ -17,25 +24,79 @@ const gameBoard = (function () {
     c3: [0, 2],
   };
 
-  const addPiece = (coordinates, player) => {
+  const addMarkToBoard = (coordinates, player) => {
     const [row, col] = coordMap[coordinates];
 
-    if (player === 1) {
-      board[row][col] = "o";
-    } else if (player === 2) {
-      board[row][col] = "x";
-    } else throw new Error("Invalid player number");
+    if (board[row][col].getValue() !== 0) {
+      console.warn("occupied square picked. nothing happened");
+      return;
+    }
+
+    board[row][col].markSquare(player.getPlayerNumber());
   };
 
-  const getBoard = () => {
-    return board;
+  const getBoard = () => board;
+
+  const printBoard = () => {
+    const processedBoard = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.table(processedBoard);
   };
 
-  return { addPiece, getBoard };
+  return { addMarkToBoard, getBoard, printBoard };
 })();
 
 function createPlayer(playerName, playerNumber) {
-  const playerNumber = playerNumber;
   const getPlayerNumber = () => playerNumber;
   return { playerName, getPlayerNumber };
 }
+
+function square() {
+  let value = 0;
+
+  const markSquare = (playerNumber) => {
+    value = playerNumber;
+  };
+
+  const getValue = () => value;
+
+  return { markSquare, getValue };
+}
+
+const gameController = (function (
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) {
+  const players = [
+    createPlayer(playerOneName, 1),
+    createPlayer(playerTwoName, 2),
+  ];
+
+  let activePlayer = players[0];
+
+  const getActivePlayer = () => activePlayer;
+
+  const switchActivePlayer = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const printTurn = () => {
+    gameBoard.printBoard();
+    console.log(`${getActivePlayer().playerName}'s turn! ↓`);
+  };
+
+  const playTurn = (coords) => {
+    gameBoard.addMarkToBoard(coords, getActivePlayer());
+    switchActivePlayer();
+    printTurn();
+  };
+
+  return { getActivePlayer, switchActivePlayer, printTurn, playTurn };
+})();
+
+gameController.printTurn();
+gameController.playTurn("a3");
+gameController.playTurn("a1");
+gameController.playTurn("a2");
+gameController.playTurn("a2");
